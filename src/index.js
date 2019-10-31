6db7f0a33bc62a8825b5588367be6e51b40e5bd6
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-10-31 16:17:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false
 
 document.addEventListener("DOMContentLoaded", ()=>{
+  // define element variables
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
   const form = document.querySelector(".add-toy-form")
@@ -16,6 +17,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // create card div
     let card = document.createElement("div")
     card.className = 'card'
+    // SET CARD DATA-ID!
     card.dataset.id=toy.id
 
     // create name header
@@ -42,6 +44,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let likeButton = document.createElement("button")
     likeButton.className = 'like-button'
     likeButton.innerText = "Like Me!"
+    // append like button to card
     card.appendChild(likeButton)
 
 
@@ -49,16 +52,15 @@ document.addEventListener("DOMContentLoaded", ()=>{
     toyCollection.appendChild(card)
   }
 
-
-
-  
+  // fetch all toys
   fetch('http://localhost:3000/toys')
   .then(response => response.json())
   .then((newToy)=>{
-    console.log(newToy)
+    // append toys
     newToy.forEach(toy =>{
       appendToy(toy)
     })
+    // add like listeners to like buttons on toy cards
     addLikeListeners()
   })
 
@@ -74,20 +76,27 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
   })
 
+  // add new toy to api
   function newToy(){
+  // add conditional bc:
+  // cannot add event listener to form if form is "add toy" button has not been clicked
   if (form){
   form.addEventListener("submit", (e)=>{
+    // prevent form submission
     e.preventDefault()
 
+    // get form values
     let newToyName = e.target.name.value
     let newToyImg = e.target.image.value
 
+    // create formData object
     let formData = {
       name: newToyName,
       image: newToyImg,
       likes: 0
     }
 
+    // create fetch configObj
     let configObj = {
       method: "POST",
       headers: {
@@ -97,6 +106,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
       body: JSON.stringify(formData)
     }
 
+    // enact & parse fetch
     fetch('http://localhost:3000/toys', configObj)
     .then(response => response.json())
     .then((json)=>{
@@ -109,35 +119,41 @@ document.addEventListener("DOMContentLoaded", ()=>{
   // * A patch request sent to the server at `http://localhost:3000/toys/:id` updating the number of likes that the specific toy has
   // * Headers and body are provided below (If your request isn't working, make sure your header and keys match the documentation.)
   
+  
   function addLikeListeners(){
+    // collect all like buttons
     let likeButts = toyCollection.getElementsByClassName("like-button")
+    // iterate over buttons 
     for (let button of likeButts){
+      // get button id
       let id = button.parentElement.dataset.id
+      // access like element
       let like = button.previousElementSibling
+      // get like count and parse into integer
       let likeCount = parseInt(like.innerText)
 
+        // attach click event handler to each like button 
          button.addEventListener("click", (event)=>{
+           // if clicked, increase likeCunt by 1
           likeCount++
-          // console.log(likeCount)
+          // enact & parse fetch to patch likes: count in api
           fetch('http://localhost:3000/toys/' + id,{ 
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-          },
-          body: JSON.stringify({
-        "likes": likeCount
-      })
-    })
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+            body: JSON.stringify({
+              "likes": likeCount
+            })
+          })
           .then(response => response.json())
           .then(updatedCard =>{
+            // assign updated likeCount to innertext of like element
             button.previousElementSibling.innerText = likeCount
-            console.log(updatedCard)
           })
         })
-      }
-    
-}
-
+      }  
+  }
 
 })
