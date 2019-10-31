7ade794405243b3d1eb919b07771e486e4d3928d
Gaida Escobar <gescobar129@gmail.com>
2019-10-31 14:08:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
-let addToy = false
+// let addToy = false
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
   const toyForm = document.querySelector('.container')
-  addBtn.addEventListener('click', () => {
-    // hide & seek with the form
-    addToy = !addToy
-    if (addToy) {
-      toyForm.style.display = 'block'
-    } else {
-      toyForm.style.display = 'none'
-    }
+  
+
+  fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(toysArray =>{
+    toysArray.forEach( toy => { addToy(toy) })
   })
 
+
+  addBtn.addEventListener('click', addToy)
+
 })
+
+function addToy(toy){
+
+  let parentDiv = document.querySelector("#collection")
+  let div = document.createElement("div")
+  // div.classList("card")
+  div.className = "card"
+  let h2 = document.createElement("h2")
+  h2.innerText = toy.name
+  let img = document.createElement("img")
+  img.src = `${toy.image}`
+  img.className = "toy-avatar"
+  let p = document.createElement("p")
+  p.innerText= "Likes"
+  let button = document.createElement("button")
+  button.innerText = toy.likes
+  button.className = "like-btn";
+   parentDiv.append(div)
+  div.append(h2)
+  div.append(img)
+  div.append(p)
+  div.append(button)
+  
+  // <div class="card">
+  //   <h2>(title from json) = Woody</h2>
+  //   <img src=toy_image_url =(image source)json class="toy-avatar" />
+  //   <p>4 Likes (likes from json)</p>
+  //   <button class="like-btn">Like <3</button>
+  // </div>
+}
